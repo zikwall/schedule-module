54dff2287cc82d633967a835c9857f7a5d7ce5e0
zikwall <andrey.kapitonov.96@gmail.com>
2018-05-03 16:13:05 +0400

    add transliterate

---
+++ components/Transliterate.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace humhub\modules\schedule\components;
+
+/**
+ * ToDo: create automatically translate from Russian chars
+ *
+ * Class Transliterate
+ * @package humhub\modules\schedule\components
+ */
+class Transliterate
+{
+    /**
+     * an array of Russian characters matched with English characters
+     */
+    const converter = [
+        'а' => 'a',   'б' => 'b',   'в' => 'v',
+        'г' => 'g',   'д' => 'd',   'е' => 'e',
+        'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
+        'и' => 'i',   'й' => 'y',   'к' => 'k',
+        'л' => 'l',   'м' => 'm',   'н' => 'n',
+        'о' => 'o',   'п' => 'p',   'р' => 'r',
+        'с' => 's',   'т' => 't',   'у' => 'u',
+        'ф' => 'f',   'х' => 'h',   'ц' => 'c',
+        'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch',
+        'ь' => '\'',  'ы' => 'y',   'ъ' => '\'',
+        'э' => 'e',   'ю' => 'yu',  'я' => 'ya',
+
+        'А' => 'A',   'Б' => 'B',   'В' => 'V',
+        'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
+        'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
+        'И' => 'I',   'Й' => 'Y',   'К' => 'K',
+        'Л' => 'L',   'М' => 'M',   'Н' => 'N',
+        'О' => 'O',   'П' => 'P',   'Р' => 'R',
+        'С' => 'S',   'Т' => 'T',   'У' => 'U',
+        'Ф' => 'F',   'Х' => 'H',   'Ц' => 'C',
+        'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Sch',
+        'Ь' => '\'',  'Ы' => 'Y',   'Ъ' => '\'',
+        'Э' => 'E',   'Ю' => 'Yu',  'Я' => 'Ya',
+    ];
+
+    /**
+     * @param $string
+     * @return string
+     */
+    public static function rus2translit($string)
+    {
+        return strtr($string, static::converter);
+    }
+
+    /**
+     * @param $str
+     * @return string
+     */
+    public static function str2clear($str)
+    {
+        $str = strtolower(static::rus2translit($str));
+        return trim(preg_replace('~[^-a-z0-9_]+~u', '-', $str), "-");
+    }
+}
